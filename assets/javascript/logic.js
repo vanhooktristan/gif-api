1643943c6ba0dfa78fa5a99257c8e3d8f68555e7
vanhooktristan <34349817+vanhooktristan@users.noreply.github.com>
2018-02-24 17:32:43 -0600

    first upload

---
+++ assets/javascript/logic.js
@@ -0,0 +1,97 @@
+// Tristan Van Hook
+// Ajax Homework
+// February 24, 2018
+// University of Kansas Coding Bootcamp
+
+$( document ).ready(function() {
+    // An array of activities, new activities will be pushed into this array;
+    var activities = ["Dancing", "Jogging", "Running", "Soccer", "Fishing", "Swimming", "Climbing", "Skipping", "Jump Rope", "Winking","Walking", "Golf", "Football"];
+    // Creating Functions & Methods
+    // Function that displays all gif buttons
+    function displayGifButtons(){
+        $("#gifButtonsView").empty(); // erasing anything in this div id so that it doesnt duplicate the results
+        for (var i = 0; i < activities.length; i++){
+            var gifButton = $("<button>");
+            gifButton.addClass("action");
+            gifButton.addClass("btn btn-primary")
+            gifButton.attr("data-name", activities[i]);
+            gifButton.text(activities[i]);
+            $("#gifButtonsView").append(gifButton);
+        }
+    }
+    // Function to add a new action button
+    function addNewButton(){
+        $("#addGif").on("click", function(){
+        var action = $("#action-input").val().trim();
+        if (action == ""){
+          return false; // added so user cannot add a blank button
+        }
+        activities.push(action);
+        $("#action-input").val("");
+    
+        displayGifButtons();
+        return false;
+        });
+    }
+    // function resets all new buttons
+    function resetButtons(){
+        $("removeGif").on("click", function(){
+        activities.pop(action);
+        displayGifButtons();
+        return false;
+        });
+    }
+    // Function that displays all of the gifs
+    function displayGifs(){
+        var action = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + action + "&api_key=dc6zaTOxFJmzC&limit=10";
+        console.log(queryURL); // displays the constructed url
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        .done(function(response) {
+            console.log(response); // console test to make sure something returns
+            $("#gifsView").empty(); // erasing anything in this div id so that it doesnt keep any from the previous click
+            var results = response.data; //shows results of gifs
+            if (results == ""){
+              alert("There isn't a gif for this selected button");
+            }
+            for (var i=0; i<results.length; i++){
+    
+                var gifDiv = $("<div>"); //div for the gifs to go inside
+                gifDiv.addClass("gifDiv");
+                // pulling rating of gif
+                var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                gifDiv.append(gifRating);
+                // pulling gif
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
+                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image
+                gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image
+                gifImage.attr("data-state", "still"); // set the image state
+                gifImage.addClass("image");
+                gifDiv.append(gifImage);
+                // pulling still image of gif
+                // adding div of gifs to gifsView div
+                $("#gifsView").prepend(gifDiv);
+            }
+        });
+    }
+    // Calling Functions & Methods
+    displayGifButtons(); // displays list of actions already created
+    addNewButton();
+    resetButtons();
+    // Document Event Listeners
+    $(document).on("click", ".action", displayGifs);
+    $(document).on("click", ".image", function(){
+        var state = $(this).attr('data-state');
+        if ( state == 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    });
+    });
